532fe03672e9281831438254eb25f8a128764212
hackerzhut <hackerzhut.inc@gmail.com>
2014-04-08 22:35:30 +0800

    Source - First Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Shuffling a deck of cards
+=========================
+
+This code sample solves a coding problem that involves shuffling a deck of cards to original order as described by the problem definition below,
+
+
+=========================
+
+You are given a deck containing n cards.  While holding the deck:
+
+1. Take the top card off the deck and set it on the table.
+2. Take the next card off the top and put it on the bottom of the deck in your hand.
+3. Continue steps 1 and 2 until all cards are on the table.  This is a round.
+4. Pick up the deck from the table and repeat steps 1-3 until the deck is in the original order.
+
+Write a program to determine how many rounds it will take to put a deck back into the original order.  This will involve creating a data structure to represent the order of the cards. This program should be written in JavaScript using Node.js  and executable from the command line. Do not use any Array methods. It should take a number of cards in the deck as a command line argument and write the result to stdout.
+
+=========================
---
+++ deck.js
@@ -0,0 +1,80 @@
+module.exports = function(deck){
+
+	"use strict";
+
+	this._tries = 0,
+	this._originalDeck = JSON.parse(JSON.stringify(deck)),
+	this._deckInHand  = [],
+	this._deckOnTable = [];
+
+
+	Array.prototype.reverse = function(){
+		for(var left=0, right = this.length-1; left < right; left++, right--){
+			var temp = this[right];
+			this[right] = this[left];
+			this[left] = temp;
+		};
+	};
+
+	Array.prototype.pop = function(index){
+		if(index < 0 || index > this.length-1) return;
+		var elem = this[index];
+		for(var _index=0, _nIndex=0; _index < this.length; _index++){
+			if(this[_index]!== elem)
+				this[_nIndex++] = this[_index];
+		};
+		this.length = _nIndex;
+	};
+
+	Array.prototype.popAndPush = function(elementToPopAndPush){
+		var elementIndex = this.indexOf(elementToPopAndPush);
+		if(elementIndex == -1)return;
+		for(var _index=elementIndex; _index < this.length; _index++){
+			var _nextElement = this[_index+1];
+			this[_index] = (_nextElement === undefined) ? elementToPopAndPush : _nextElement;
+		};
+	};
+
+	Array.prototype.compare = function(arr){
+		var that = this,
+		arrayLength = arr.length;
+		if(this.length != arrayLength){
+			return false;
+		}
+		var isEqual = arr.every(function(elem, _index){
+			// Assuming array don't have arrays as elements
+			if(that[_index] != arr[_index]){
+				return false;
+			}
+			return true;
+		});
+		return isEqual;
+	};
+
+	this.shuffleToOriginal = function(){
+		while(true){
+			while(deck.length > 0){
+				//placing top element on the table
+				this._deckOnTable.push(deck[0]); 
+				deck.pop(0); //Pop the top card
+				if(deck.length == 0) break;
+				//take the next top element and place it under the deck in hand
+				deck.popAndPush(deck[0]); 
+			}
+			//Reverse the table to restore the order			
+			this._deckOnTable.reverse();
+			this._tries++;
+			//Reset the values
+			deck = this._deckOnTable;
+			this._deckOnTable = [];
+			console.log('[ End of Round '+ (this._tries)+' : deck on table: '+deck+' ]');
+			//Outerloop break condition
+			if(deck.compare(this._originalDeck)){
+				break;
+			}
+		};
+		return this._tries;
+	};
+};
+
+
